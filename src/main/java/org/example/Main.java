684395e42fbcc0377e5d8f1b9a81dec983cd34ee
joshfeds <joshfeds14@gmail.com>
2024-02-21 16:25:04 -0500

    Getting started

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int num_guests = 5;
+    Thread servant = new Thread();
+    Thread[] guests = new Thread[num_guests];
+    }
+}
