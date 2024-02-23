f44914cffff4a9f52c7060e01485df854313a232
joshfeds <joshfeds14@gmail.com>
2024-02-22 23:58:12 -0500

    Fixed up first problem

--- src/main/java/Cupcake.java
+++ src/main/java/Cupcake.java
@@ -3,74 +3,77 @@ import java.util.concurrent.Semaphore;
 
 
 public class Cupcake {
-    public static int num_guests = 5;
+    public static int num_guests = 10;
     private static boolean[] eaten = new boolean[num_guests];
     private static int plateCount = 0;
     private static boolean cupcakeAvailable = true;
     private static int currentGuest;
     private static boolean isComplete = false;
     private static final Semaphore sem = new Semaphore(1);
+    private static int totalEntered = 0;
 
-
-    public static void enterLabyrinth(int index) throws InterruptedException {
-        while(plateCount < num_guests){
+    public static void enterLabyrinth() throws InterruptedException {
+        int max = num_guests;
+        int min = 0;
+        totalEntered++;
+        while (plateCount < num_guests) {
 
             sem.acquire();
+            currentGuest = (int) ((Math.random() * (max - min)) + min);
 
-            if(cupcakeAvailable && !eaten[index]){
+            if (cupcakeAvailable && !eaten[currentGuest]) {
                 cupcakeAvailable = false;
-                eaten[index] = true;
-                System.out.println("Guest #" + index + " ate the cupcake!");
+                eaten[currentGuest] = true;
+                System.out.println("Guest #" + currentGuest + " ate the cupcake!");
             }
 
-            if(currentGuest == 0 && !cupcakeAvailable){
+            if (currentGuest == 0 && !cupcakeAvailable) {
                 plateCount++;
                 cupcakeAvailable = true;
-                System.out.println("The leader counts an empty plate");
+                System.out.println("The leader counts an empty plate and replaces it with a cupcake");
+
+            }
+
+            if(eaten[currentGuest] && cupcakeAvailable){
+                System.out.println("Guest #" + currentGuest + " can't eat anymore");
             }
             sem.release();
+
+
         }
-        isComplete = true;
 
     }
+
     public static void main(String[] args) throws InterruptedException {
 
-    Thread[] guests = new Thread[num_guests];
-    int max = num_guests - 1;
-    int min = 0;
+        Thread[] guests = new Thread[num_guests];
 
-    List<Integer> randList = new ArrayList<>();
-    for(int i = 0; i < num_guests; i++) {
-        randList.add(i);
-    }
+        List<Integer> randList = new ArrayList<>();
+        for (int i = 0; i < num_guests; i++) {
+            randList.add(i);
+        }
         Collections.shuffle(randList);
-    for(int i = 0; i < num_guests; i++){
+        for (int i = 0; i < num_guests; i++) {
+            int index = randList.get(i);
+            guests[index] = new Thread(() -> {
+                try {
+                    enterLabyrinth();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
 
-        int index = randList.get(i);
+        for (Thread thread : guests) {
+            thread.start();
+        }
 
 
-        guests[i] = new Thread(() -> {
-            try {
-                enterLabyrinth(index);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-
-    for(Thread thread : guests){
-        thread.start();
-    }
-    while(!isComplete){
-
-        currentGuest = (int) ((Math.random() * (max - min)) + min);
-
-    }
+        for (Thread thread : guests) {
+            thread.join();
+        }
 
         System.out.println("Everyone has eaten a cupcake, time to go home!");
 
     }
-
-
 }
